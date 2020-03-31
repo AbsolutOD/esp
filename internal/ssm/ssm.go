52bb3cf040e8b062edce0e647c0516454b1c7be6
Matt O'Donnell <matt@instridetechnologies.com>
2020-03-31 17:07:43 -0400

    * finished refactoring things to use interfaces for the backend

--- internal/ssm/ssm.go
+++ internal/ssm/ssm.go
@@ -1,1 +1,62 @@
-package ssmparam
+package ssm
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	awsssm "github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/pinpt/esp/internal/client"
+	"github.com/pinpt/esp/internal/errors"
+	"os"
+)
+
+type Service struct {
+	Svc *awsssm.SSM
+	Region string
+	Cfg aws.Config
+	session *session.Session
+}
+
+func (s *Service) Save(p client.EspParam) (client.EspParam, error) {
+	panic("implement me")
+}
+
+func (s *Service) GetOne(p client.GetOneInput) client.EspParam {
+	si := &awsssm.GetParameterInput{
+		Name: aws.String(p.Path),
+		WithDecryption: aws.Bool(p.Decrypt),
+	}
+	resp, err := s.Svc.GetParameter(si)
+	if err != nil {
+		errors.CheckSSMGetParameters(err)
+	}
+	param := utils.ConvertToEspParam(resp)
+	return param
+}
+
+/*func GetMany(ec client.EspConfig, d bool, paths []*string) []*ssm.Parameter {
+
+	si := &ssm.GetParametersInput{
+		Names: paths,
+		WithDecryption: aws.Bool(d),
+	}
+	fmt.Println(si)
+	resp, err := ec.Svc.GetParameters(si)
+	if err != nil {
+		fmt.Println(err)
+		errors.CheckSSMGetParameters(err)
+	}
+	return resp.Parameters
+}*/
+
+// actually create the ssm client
+func New() *Service {
+	svc := new(Service)
+	svc.Region = os.Getenv("AWS_REGION")
+	svc.Cfg = aws.Config{ Region: aws.String(svc.Region) }
+	return svc
+}
+
+func (s *Service) Init() {
+	s.session = session.Must(session.NewSession(&s.Cfg))
+	s.Svc = awsssm.New(s.session)
+}
--- internal/ssm/utils.go
+++ internal/ssm/utils.go
@@ -1,1 +1,36 @@
-package ssmparam
+package ssm
+
+import (
+	"errors"
+	awsssm "github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/pinpt/esp/internal/client"
+)
+
+type ParamType string
+
+const(
+	String ParamType = "string"
+	SecureString ParamType = "securestring"
+	StringList ParamType = "stringlist"
+)
+
+type AwsParam struct {
+	Arn string
+	Name string
+	Type ParamType
+	Value string
+	Version int
+	LastModifiedDate float32
+}
+
+func (p *AwsParam) IsValid() error {
+	switch p.Type {
+	case String, SecureString, StringList:
+		return nil
+	}
+	return errors.New("invalid SSM Parameter Type")
+}
+
+func ConvertToEspParam(ap *awsssm.Parameter) client.EspParam {
+
+}
