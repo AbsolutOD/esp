5d7b75025a34ee38823ed9834977916ead93ec36
Matt O'Donnell <matt@instridetechnologies.com>
2020-04-05 11:55:57 -0400

    * added comments for the public structs

--- internal/common/common.go
+++ internal/common/common.go
@@ -2,6 +2,7 @@ package common
 
 import "time"
 
+// EspParam represents the parameter that is being managed by ESP
 type EspParam struct {
 	Id string
 	Name string
@@ -13,6 +14,7 @@ type EspParam struct {
 	LastModifiedDate time.Time
 }
 
+// EspParamInput represents parameter that is going to be saved
 type EspParamInput struct {
 	Path string
 	Name string
@@ -20,15 +22,18 @@ type EspParamInput struct {
 	Value string
 }
 
+// GetOneInput represents the query to get a param
 type GetOneInput struct {
 	Name string
 	Decrypt bool
 }
 
+// SaveOutput represents the response from a save operation
 type SaveOutput struct {
 	Version int64
 }
 
+// ListParamInput represents the output of a list query
 type ListParamInput struct {
 	Path string
 	Decrypt bool
